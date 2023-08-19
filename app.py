a4a2223c4fce682ccb479f6986c43a19f82da32f
Kevin Knights <kevinknights29@hotmail.com>
2023-08-19 16:10:53 -0500

    added list picker for language detection

--- app.py
+++ app.py
@@ -6,18 +6,38 @@ import gradio as gr
 import tesserocr
 
 
-def image_to_text(image):
-    image_text = tesserocr.image_to_text(image, lang="spa")
+def _language_matcher(language):
+    mapping = {
+        "English": "en",
+        "Spanish": "spa",
+        "Portuguese": "por",
+    }
+    if language in mapping:
+        return mapping[language]
+    raise ValueError(f"The language {language} is not mapped...")
+
+
+def image_to_text(image, language):
+    image_text = tesserocr.image_to_text(
+        image=image,
+        lang=_language_matcher(language),
+    )
     output_msg = f"Text from Image: {image_text}"
     return output_msg
 
 
 demo = gr.Interface(
     fn=image_to_text,
-    inputs=gr.Image(type="pil"),
+    inputs=[
+        gr.Image(type="pil"),
+        gr.Dropdown(choices=["English", "Spanish", "Portuguese"], label="Language"),
+    ],
     outputs=gr.Textbox(label="Text Output", placeholder="Image text will go here:"),
     examples=[
-        os.path.join(os.path.dirname(__file__), "docs/example.jpeg"),
+        [
+            os.path.join(os.path.dirname(__file__), "docs/example.jpeg"),
+            "Spanish",
+        ],
     ],
 )
 
